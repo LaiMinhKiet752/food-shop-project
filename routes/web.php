825cdb47f0baafd0ae68c54c6a13269acee866ac
Minh Kiet <kietminh070502@gmail.com>
2023-03-17 20:30:42 +0700

    Edit the layout for the admin page

--- routes/web.php
+++ routes/web.php
@@ -25,12 +25,14 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
  
-
+//Admin Dashborad
 Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
 });
 
 
+
+//Vendor Dashborad
 Route::middleware(['auth', 'role:vendor'])->group(function () {
     Route::get('/vendor/dashboard', [VendorController::class, 'VendorDashboard'])->name('vendor.dashboard');
 });
