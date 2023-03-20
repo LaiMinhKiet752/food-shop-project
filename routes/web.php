f9db65b3a3396fe36f925972ae4873de4b0756c6
Minh Kiet <kietminh070502@gmail.com>
2023-03-20 20:23:51 +0700

    Vendor Logout Option

--- routes/web.php
+++ routes/web.php
@@ -24,15 +24,15 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
- 
+
 //Admin Dashborad
 Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
-     Route::get('/admin/logout', [AdminController::class, 'AdminDestroy'])->name('admin.logout');
-     Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
-     Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
-     Route::get('/admin/change/password', [AdminController::class, 'AdminChangePassword'])->name('admin.change.password');
-     Route::post('/admin/update/password', [AdminController::class, 'AdminUpdatePassword'])->name('update.password');
+    Route::get('/admin/logout', [AdminController::class, 'AdminDestroy'])->name('admin.logout');
+    Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
+    Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
+    Route::get('/admin/change/password', [AdminController::class, 'AdminChangePassword'])->name('admin.change.password');
+    Route::post('/admin/update/password', [AdminController::class, 'AdminUpdatePassword'])->name('update.password');
 });
 
 
@@ -40,10 +40,12 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
 //Vendor Dashborad
 Route::middleware(['auth', 'role:vendor'])->group(function () {
     Route::get('/vendor/dashboard', [VendorController::class, 'VendorDashboard'])->name('vendor.dashboard');
+    Route::get('/vendor/logout', [VendorController::class, 'VendorDestroy'])->name('vendor.logout');
 });
 
 
 Route::get('/admin/login', [AdminController::class, 'AdminLogin']);
+Route::get('/vendor/login', [VendorController::class, 'VendorLogin']);
 
 //Edit Profile
 Route::middleware('auth')->group(function () {
