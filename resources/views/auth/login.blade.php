25ab2a67af3283c6dd4acac65b52abe3e24b25c5
Minh Kiet <kietminh070502@gmail.com>
2023-04-01 10:34:22 +0700

    User Reset Password Setup

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -46,14 +46,14 @@
                                     <div class="padding_eight_all bg-white">
                                         <div class="heading_s1">
                                             <h1 class="mb-5">Login</h1>
-                                            <p class="mb-30">Don't have an account? <a href="page-register.html">Create
+                                            <p class="mb-30">Don't have an account? <a href="{{ route('register') }}">Create
                                                     here</a></p>
                                         </div>
                                         <form method="POST" action="{{ route('login') }}">
                                             @csrf
                                             <div class="form-group">
                                                 <input type="email" id="email" required="" name="email"
-                                                    placeholder="Username or Email *" />
+                                                    placeholder="Your email *" />
                                             </div>
                                             <div class="form-group">
                                                 <input required="" id="password" type="password" name="password"
