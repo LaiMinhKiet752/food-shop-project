af863bb0ef83d386f029caa4f5cca05b2def3d9b
Minh Kiet <kietminh070502@gmail.com>
2023-05-10 10:14:30 +0700

    Show Image Using Javascript

--- resources/views/backend/product/product_add.blade.php
+++ resources/views/backend/product/product_add.blade.php
@@ -1,5 +1,6 @@
 @extends('admin.admin_dashboard')
 @section('admin')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <div class="page-content">
         <!--breadcrumb-->
         <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
@@ -55,17 +56,24 @@
                                     <label for="inputProductDescription" class="form-label">Long Description *</label>
                                     <textarea id="mytextarea" name="long_description"></textarea>
                                 </div>
-
+                                <br>
                                 <div class="mb-3">
                                     <label for="inputProductTitle" class="form-label">Main Thumbnail *</label>
-                                    <input name="product_thumbnail" class="form-control" type="file" id="formFile">
+                                    <input name="product_thumbnail" class="form-control" type="file" id="formFile"
+                                        onchange="mainThumbnailUrl(this)"><br>
+                                    <img src="" id="mainthumbnail" alt="">
                                 </div>
 
                                 <div class="mb-3">
                                     <label for="inputProductTitle" class="form-label">Multiple Image *</label>
-                                    <input name="multiple_image[]" class="form-control" type="file" id="formFileMultiple"
+                                    <input class="form-control" name="multiple_image[]" type="file" id="multipleImage"
                                         multiple="">
+                                    <br>
+                                    <div class="row" id="preview_image">
+
+                                    </div>
                                 </div>
+
                             </div>
                         </div>
                         <div class="col-lg-4">
@@ -197,4 +205,49 @@
         </div>
 
     </div>
+
+
+    <script type="text/javascript">
+        function mainThumbnailUrl(input) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+                reader.onload = function(e) {
+                    $('#mainthumbnail').attr('src', e.target.result).width(150).height(140);
+                };
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+    </script>
+
+    <script>
+        $(document).ready(function() {
+            $('#multipleImage').on('change', function() { //on file input change
+                if (window.File && window.FileReader && window.FileList && window
+                    .Blob) //check File API supported browser
+                {
+                    var data = $(this)[0].files; //this file data
+
+                    $.each(data, function(index, file) { //loop though each file
+                        if (/(\.|\/)(gif|jpe?g|png)$/i.test(file
+                                .type)) { //check supported file type
+                            var fRead = new FileReader(); //new filereader
+                            fRead.onload = (function(file) { //trigger function on successful read
+                                return function(e) {
+                                    var img = $('<img/>').addClass('thumb').attr('src',
+                                            e.target.result).width(140)
+                                        .height(140); //create image element
+                                    $('#preview_image').append(
+                                        img); //append image to output element
+                                };
+                            })(file);
+                            fRead.readAsDataURL(file); //URL representing the file's data.
+                        }
+                    });
+
+                } else {
+                    alert("Your browser doesn't support File API!"); //if File API is absent
+                }
+            });
+        });
+    </script>
 @endsection
