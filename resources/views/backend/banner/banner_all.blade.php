e801fd4ee60aba63b34c6e011055f2e9d1ab85bd
Phucle2705 <0306201275@caothang.edu.vn>
2023-05-15 11:21:32 +0700

    Backend Banner CRUD Setup Part 3

--- resources/views/backend/banner/banner_all.blade.php
+++ resources/views/backend/banner/banner_all.blade.php
@@ -45,8 +45,8 @@
                                     </td>
 
                                     <td>
-                                        <a href="{{ route('edit.slider', $item->id) }}" class="btn btn-info">Edit</a>
-                                        <a href="{{ route('delete.slider', $item->id) }}" class="btn btn-danger"
+                                        <a href="{{ route('edit.banner', $item->id) }}" class="btn btn-info">Edit</a>
+                                        <a href="{{ route('delete.banner', $item->id) }}" class="btn btn-danger"
                                             id="delete">Delete</a>
 
                                     </td>
