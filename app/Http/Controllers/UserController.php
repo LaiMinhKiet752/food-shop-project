26c6b989135c35f17369a94c84d73a283fc9b9d8
Minh Kiet <kietminh070502@gmail.com>
2023-05-31 18:48:22 +0700

    Fixed Some Bugs In User Update Profile

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,32 +20,112 @@ class UserController extends Controller
         $id = Auth::user()->id;
         $data = User::find($id);
         $data->name = $request->name;
-        $data->email = $request->email;
-        $data->phone = $request->phone;
         $data->address = $request->address;
 
-        if ($request->file('photo')) {
+        $current_phone_number = User::find($id)->phone;
+        $current_email = User::find($id)->email;
+        if ($current_phone_number == $request->phone && $current_email == $request->email) {
+            if ($request->file('photo')) {
+                $request->validate([
+                    'photo' => 'image|max:2048'
+                ], [
+                    'photo.image' => 'The uploaded file must be an image in one of the following formats: jpg, jpeg, png, bmp, gif, svg, or webp.',
+                    'photo.max' => 'Maximum image size is 2MB.',
+                ]);
+                $file = $request->file('photo');
+                $filename = hexdec(uniqid()) . '_user' . '.' . $file->getClientOriginalExtension();
+                @unlink(public_path('upload/user_images/' . $data->photo));
+                $file->move(public_path('upload/user_images/'), $filename);
+                $data['photo'] = $filename;
+            }
+            $data->save();
+            $notification = array(
+                'message' => 'User Profile Updated Successfully!',
+                'alert-type' => 'success',
+            );
+            return redirect()->back()->with($notification);
+        } else if ($current_phone_number != $request->phone && $current_email == $request->email) {
             $request->validate([
-                'photo' => 'image|max:2048'
+                'phone' => ['unique:' . User::class],
             ], [
-                'photo.image' => 'The uploaded file must be an image in one of the following formats: jpg, jpeg, png, bmp, gif, svg, or webp.',
-                'photo.max' => 'Maximum image size is 2MB.',
+                'phone.unique' => 'The phone number already exists. Please enter another phone number.'
             ]);
-            $file = $request->file('photo');
-            $filename = hexdec(uniqid()) . '_user' . '.' . $file->getClientOriginalExtension();
-            @unlink(public_path('upload/user_images/' . $data->photo));
-            $file->move(public_path('upload/user_images/'), $filename);
-            $data['photo'] = $filename;
+            if ($request->file('photo')) {
+                $request->validate([
+                    'photo' => 'image|max:2048'
+                ], [
+                    'photo.image' => 'The uploaded file must be an image in one of the following formats: jpg, jpeg, png, bmp, gif, svg, or webp.',
+                    'photo.max' => 'Maximum image size is 2MB.',
+                ]);
+                $file = $request->file('photo');
+                $filename = hexdec(uniqid()) . '_user' . '.' . $file->getClientOriginalExtension();
+                @unlink(public_path('upload/user_images/' . $data->photo));
+                $file->move(public_path('upload/user_images/'), $filename);
+                $data['photo'] = $filename;
+            }
+            $data->phone = $request->phone;
+            $data->save();
+            $notification = array(
+                'message' => 'User Profile Updated Successfully!',
+                'alert-type' => 'success',
+            );
+            return redirect()->back()->with($notification);
+        } else if ($current_phone_number == $request->phone && $current_email != $request->email) {
+            $request->validate([
+                'email' => ['unique:' . User::class],
+            ], [
+                'email.unique' => 'The email already exists. Please enter another email.'
+            ]);
+            if ($request->file('photo')) {
+                $request->validate([
+                    'photo' => 'image|max:2048'
+                ], [
+                    'photo.image' => 'The uploaded file must be an image in one of the following formats: jpg, jpeg, png, bmp, gif, svg, or webp.',
+                    'photo.max' => 'Maximum image size is 2MB.',
+                ]);
+                $file = $request->file('photo');
+                $filename = hexdec(uniqid()) . '_user' . '.' . $file->getClientOriginalExtension();
+                @unlink(public_path('upload/user_images/' . $data->photo));
+                $file->move(public_path('upload/user_images/'), $filename);
+                $data['photo'] = $filename;
+            }
+            $data->email = $request->email;
+            $data->save();
+            $notification = array(
+                'message' => 'User Profile Updated Successfully!',
+                'alert-type' => 'success',
+            );
+            return redirect()->back()->with($notification);
+        } else if ($current_phone_number != $request->phone && $current_email != $request->email) {
+            $request->validate([
+                'email' => ['unique:' . User::class],
+                'phone' => ['unique:' . User::class],
+            ], [
+                'email.unique' => 'The email already exists. Please enter another email.',
+                'phone.unique' => 'The phone number already exists. Please enter another phone number.'
+            ]);
+            if ($request->file('photo')) {
+                $request->validate([
+                    'photo' => 'image|max:2048'
+                ], [
+                    'photo.image' => 'The uploaded file must be an image in one of the following formats: jpg, jpeg, png, bmp, gif, svg, or webp.',
+                    'photo.max' => 'Maximum image size is 2MB.',
+                ]);
+                $file = $request->file('photo');
+                $filename = hexdec(uniqid()) . '_user' . '.' . $file->getClientOriginalExtension();
+                @unlink(public_path('upload/user_images/' . $data->photo));
+                $file->move(public_path('upload/user_images/'), $filename);
+                $data['photo'] = $filename;
+            }
+            $data->email = $request->email;
+            $data->phone = $request->phone;
+            $data->save();
+            $notification = array(
+                'message' => 'User Profile Updated Successfully!',
+                'alert-type' => 'success',
+            );
+            return redirect()->back()->with($notification);
         }
-
-        $data->save();
-
-        $notification = array(
-            'message' => 'User Profile Updated Successfully!',
-            'alert-type' => 'success',
-        );
-
-        return redirect()->back()->with($notification);
     } // End Mehtod
     public function UserLogout(Request $request)
     {
