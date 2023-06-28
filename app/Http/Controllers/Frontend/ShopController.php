11427a1e09238799c6ebb549a855afdf68aad094
Minh Kiet <kietminh070502@gmail.com>
2023-06-28 16:29:04 +0700

    Product Filter By Brand

--- app/Http/Controllers/Frontend/ShopController.php
+++ app/Http/Controllers/Frontend/ShopController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Brand;
 use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
@@ -16,20 +17,27 @@ class ShopController extends Controller
             $slugs = explode(',', $_GET['category']);
             $category_id = Category::select('id')->whereIn('category_slug', $slugs)->pluck('id')->toArray();
             $products = Product::whereIn('category_id', $category_id)->get();
+        } else if (!empty($_GET['brand'])) {
+            $slugs = explode(',', $_GET['brand']);
+            $brand_id = Brand::select('id')->whereIn('brand_slug', $slugs)->pluck('id')->toArray();
+            $products = Product::whereIn('brand_id', $brand_id)->get();
         } else {
             $products = Product::where('status', 1)->orderBy('id', 'DESC')->get();
         }
         $categories = Category::orderBy('category_name', 'ASC')->get();
+        $brands = Brand::orderBy('brand_name', 'ASC')->get();
         $newProduct = Product::where('status', 1)->orderBy('id', 'DESC')->limit(3)->get();
-        return view('frontend.product.shop_page', compact('products', 'categories', 'newProduct'));
+        return view('frontend.product.shop_page', compact('products', 'categories', 'brands', 'newProduct'));
     } //End Method
 
     public function ShopFilter(Request $request)
     {
         $data = $request->all();
 
-        //Filter By Category
+        //Filter By Category And Filter By Brand    
         $category_url = "";
+        $brand_url = "";
+
         if (!empty($data['category'])) {
             foreach ($data['category'] as $category) {
                 if (empty($category_url)) {
@@ -39,6 +47,15 @@ class ShopController extends Controller
                 }
             }
             return redirect()->route('shop.page', $category_url);
+        } else if (!empty($data['brand'])) {
+            foreach ($data['brand'] as $brand) {
+                if (empty($brand_url)) {
+                    $brand_url .= '&brand=' . $brand;
+                } else {
+                    $brand_url .= ',' . $brand;
+                }
+            }
+            return redirect()->route('shop.page', $brand_url);
         } else {
             return redirect()->route('shop.page');
         }
